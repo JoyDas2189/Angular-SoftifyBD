f12b59277bc130c24797211e38f79237d680750a
Joy Das <dasjoy365d@gmail.com>
2024-08-06 17:43:55 +0600

    String Interpolation, Property Binding, Event Binding added

--- test-app/src/app/test/test.component.ts
+++ test-app/src/app/test/test.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { log } from 'console';
 
 @Component({
   selector: 'app-test',
   templateUrl: './test.component.html',
-  styleUrl: './test.component.css'
+  styleUrl: './test.component.css',
 })
 export class TestComponent {
+  interpolation: string = 'Using String Interpolation';
+  propBinding: string = 'https://www.google.co.uk/';
+  searchValue: string = '';
 
+  changesearchValue(eventData: Event) {
+    this.searchValue = (<HTMLInputElement>eventData.target).value;
+  }
 }
